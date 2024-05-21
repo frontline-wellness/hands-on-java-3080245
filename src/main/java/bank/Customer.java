e4b1fcaeffb1fd6889db02786cbd71e088ce32db
frontline-wellness <168108987+frontline-wellness@users.noreply.github.com>
2024-05-21 14:34:31 +0000

    Create authenticator

--- src/main/java/bank/Customer.java
+++ src/main/java/bank/Customer.java
@@ -1,20 +1,21 @@
 package bank;
 
 public class Customer {
-   private int id;
-   private String name;
-   private String username;
-   private String password;
-   private int accountId;
+  private int id;
+  private String name;
+  private String username;
+  private String password;
+  private int accountId;
+  private boolean authenticated;
 
-   public Customer(int id, String name, String username, String password, int accountId){
+  public Customer(int id, String name, String username, String password, int accountId) {
     setId(id);
     setName(name);
     setUsername(username);
     setPassword(password);
     setAccountId(accountId);
-   }
-
+    setAuthenticated(false);
+  }
 
   public int getId() {
     return this.id;
@@ -55,5 +56,15 @@ public class Customer {
   public void setAccountId(int accountId) {
     this.accountId = accountId;
   }
+
+
+  public boolean isAuthenticated() {
+    return this.authenticated;
+  }
+
+  public void setAuthenticated(boolean authenticated) {
+    this.authenticated = authenticated;
+  }
+
 
 }
